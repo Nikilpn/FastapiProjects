55a8476f9648fd5044cd40b7a7676d29e42e00ea
Nikilpn <nikhilpnnick1234@gmail.com>
2025-10-26 16:19:49 +0530

    updated requirements.txt

--- fastapiproject1/main.py
+++ fastapiproject1/main.py
@@ -4,12 +4,39 @@ app=FastAPI()
 
 @app.get('/')
 def Index():
-    return "hello world"
+    return {'data':'blog list'}
 
 
-@app.get('/dict')
-def Indexx():
-    return {'data':{'name':'nikhil'}}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {}
 
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments():
+    return {'data'}
+
+
+
+
+
+@app.get('/about')
 def about_page():
     return {"data":"This is my about pages"}
+
+@app.get('about/same')
+def about_page():
+    return {"data":"this iis also my about Page"}
+
+# print("hwlli")
+# one=[1,23,4]
+# for i in one:
+#     if i==1:
+#         print("we got one")
+
+#     if i==4:
+#         print("i is 4")
